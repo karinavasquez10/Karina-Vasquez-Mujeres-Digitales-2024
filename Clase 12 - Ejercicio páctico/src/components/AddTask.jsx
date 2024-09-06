b2be0c415accf7673f248c01b8762e9188b35ed8
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2024-09-05 19:10:02 -0500

    add: se actualiza a la clase 12

---
+++ Clase 12 - Ejercicio páctico/src/components/AddTask.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+const AddTask = ({ listTask, setlistTask }) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const [title, description] = e.target;
+        setlistTask([...listTask, { title, description }]);
+    };
+
+    return (
+        <section>
+            <h1>Agregar una tarea</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" />
+                <textarea></textarea>
+
+                <button type="submit">Agregar</button>
+            </form>
+        </section>
+    );
+};
+
+AddTask.propTypes = {
+    listTask: PropTypes.array.isRequired,
+    setlistTask: PropTypes.func.isRequired,
+};
+
+export default AddTask;
