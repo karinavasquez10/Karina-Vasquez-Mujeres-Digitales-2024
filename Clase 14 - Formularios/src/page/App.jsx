0f820575b2deda6f620b8db96c5d8319301b5b73
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2024-09-11 21:01:15 -0500

    add: se agrega la clase 14 completa

--- Clase 14 - Formularios/src/page/App.jsx
+++ Clase 14 - Formularios/src/page/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function App() {
     const [odontologo, setOdontologo] = useState({ nombre: null, apellido: null, matricula: null });
+
     // Estados para el imput de nombre
     const [name, setName] = useState("");
     const [nameClass, setNameClass] = useState("");
@@ -14,30 +15,95 @@ function App() {
     const [code, setCode] = useState("");
     const [codeClass, setCodeClass] = useState("");
 
+    const validationText = (text) => {
+        const regex = /^[a-zA-Z\s]*$/; // ¿Cómo podemos agregar tildes en la expresión regular?
+        return regex.test(text);
+    };
+
+    const validationCode = (code) => {
+        const regex = /^[a-zA-Z0-9]+$/;
+        return regex.test(code);
+    };
+
     // * Funciones de validación de input
     // Validar nombre
     const handlerName = (event) => {
         const name = event.target.value;
-        const regex = /^[a-zA-Z\s]*$/; // ¿Cómo podemos agregar tildes en la expresión regular?
 
-        if (regex.test(name)) {
-            console.log("Nombre válido");
+        if (validationText(name)) {
             setName(name);
             setNameClass("valid");
         } else {
             setNameClass("invalid");
-            console.log("Nombre inválido");
         }
     };
 
     // Validar apellido
-    const handlerLastName = (e) => {};
+    const handlerLastName = (event) => {
+        const lastName = event.target.value;
+
+        if (validationText(lastName)) {
+            setLastName(lastName);
+            setLastNameClass("valid");
+        } else {
+            setLastNameClass("invalid");
+        }
+    };
 
     // Validar matricula
-    const handlerCode = (e) => {};
+    const handlerCode = (event) => {
+        const code = event.target.value;
+
+        if (validationCode(code)) {
+            setCode(code);
+            setCodeClass("valid");
+        } else {
+            setCodeClass("invalid");
+        }
+    };
 
     //* Validar formulario
-    const handlerSubmit = (e) => {};
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+
+        if (name === "" || name === null || !validationText(name)) {
+            alert("El nombre es inválido");
+            return;
+        }
+
+        if (lastName === "" || lastName === null || !validationText(lastName)) {
+            alert("El apellido es inválido");
+            return;
+        }
+
+        if (code === "" || code === null || !validationCode(code)) {
+            alert("La matricula es inválida");
+            return;
+        }
+
+        setOdontologo({
+            nombre: name,
+            apellido: lastName,
+            matricula: code,
+        });
+
+        const fetchData = async (URL) => {
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(odontologo),
+            };
+
+            const responde = await fetch(URL, options);
+            const res = await responde.json();
+
+            console.log(res);
+        };
+
+        fetchData("http://localhost:8080/odontologo/guardar");
+    };
 
     return (
         <>
